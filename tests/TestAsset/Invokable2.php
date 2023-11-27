a9eecc3b62b08fa1826eee52f3f3f7d6102ed0da
Dominik Zogg <dominik.zogg@hotmail.com>
2023-11-27 21:36:40 +0100

    php 8.3

--- tests/TestAsset/Invokable2.php
+++ tests/TestAsset/Invokable2.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Tests\Laminas\Config\TestAsset;
 
-final class Invokable2
-{
-}
+final class Invokable2 {}
--- tests/TestAsset/Invokable3.php
+++ tests/TestAsset/Invokable3.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Tests\Laminas\Config\TestAsset;
 
-final class Invokable3
-{
-}
+final class Invokable3 {}
